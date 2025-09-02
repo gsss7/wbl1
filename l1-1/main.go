a251af70a93fe8acca749c55553740c5314d4c97
Gleb <teddypro1996@gmail.com>
2025-09-02 22:54:57 +0700

    l1 done

--- l1-1/main.go
+++ l1-1/main.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+type Human struct {
+	FirstName string
+	LastName  string
+	Age       int
+	Phone     string
+}
+
+type Action struct {
+	Human
+}
+
+func (h Human) GetFullName() string { return h.FirstName + " " + h.LastName }
+
+func (h Human) GetYearOfBirth() int { return 2025 - h.Age }
+
+func (a Action) Run() {
+	fmt.Println(a.GetFullName(), "running")
+}
+
+func (a Action) Call() string {
+	return fmt.Sprintf("%s is calling: %s", a.GetFullName(), a.Phone)
+}
 func main() {
-	fmt.Println("Hello World")
+	human := Human{
+		FirstName: "James",
+		LastName:  "Bond",
+		Age:       32,
+		Phone:     "123-456-789",
+	}
+
+	action := Action{
+		Human: human,
+	}
+
+	action.Run()
+
+	fmt.Println(action.Call())
+	fmt.Println(action.GetFullName())
+	fmt.Println(action.GetYearOfBirth())
 }
