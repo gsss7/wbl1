2859c4ec19dc5536d64562053c6fe051e56055db
Gleb <teddypro1996@gmail.com>
2025-09-08 01:30:59 +0700

    l2 done

--- l1-2/main.go
+++ l1-2/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var numbers = []int{2, 4, 6, 8, 10}
+	var wg sync.WaitGroup
+	for _, num := range numbers {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			fmt.Println(n * n)
+		}(num)
+	}
+	wg.Wait()
 }
