dd77e0bcb976f895c3c5aba765810caec4e427e4
Gleb <teddypro1996@gmail.com>
2025-10-17 20:06:54 +0700

    l10 done

---
+++ l1-10/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	temperatures := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5, 0.5, -0.5}
+	groups := make(map[int][]float32)
+
+	for _, temp := range temperatures {
+		group := int(temp/10) * 10
+		groups[group] = append(groups[group], temp)
+	}
+	fmt.Println(groups)
+}
